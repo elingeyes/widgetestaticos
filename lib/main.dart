73ef2d4060d1dba0cdfbff6d804e4a69e77e3579
alexsi <alexandersilva580@gmail.com>
2025-06-22 23:16:16 -0400

    declaracion del tema de colores titulo de la app y contenido header

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,25 @@ class MyApp extends StatelessWidget {
   //inicio de la app
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    //el return es lo que devolvera en la interfaz de vista
+    return MaterialApp(
+      //title el titulo de la app
+      title: 'Formulario de Registro',
+      //declaracion del thema
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        //color primero
+        primaryColor: Color(0xFF1E3A8A),
+        //esquema de colores que tendra la app
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Color(0xFF1E3A8A),
+            secondary: Color(0xFF6B7280),
+        ),
+        useMaterial3: true,
+      ),
+      //home: RegistrationFormScreen(),
+      debugShowCheckedModeBanner: false, //desactivamos el banner por defecto de flutter
+    );
   }
 }
+
