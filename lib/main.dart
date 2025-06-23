9d13eb3291773635339125703594c921e4516ca4
alexsi <alexandersilva580@gmail.com>
2025-06-22 22:58:58 -0400

    primer commit con el inicio de la app declaracion de la libreria y funcion principal que contendra toda la app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
-//empezar el proyecto de cero
+import 'package:flutter/material.dart';
+
+//declaracion de la funcion principal
+void main(){
+  runApp(const MyApp());
+}
+
+//definion de la clase que extiende StatelessWidget
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  //inicio de la app
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
