2d7b5596174b53ce7bf23f9cc9d5a44deb5e38c8
alexsi <alexandersilva580@gmail.com>
2025-06-22 23:23:02 -0400

    declaracion de la clase RegistrationFromScreen donde se tendra todo el contenido de la app se agrego los colores que seran manejados para la interfaz

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,26 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      //home: RegistrationFormScreen(),
+      home: RegistrationFormScreen(),
       debugShowCheckedModeBanner: false, //desactivamos el banner por defecto de flutter
     );
   }
 }
 
+class RegistrationFormScreen extends StatelessWidget {
+  //definicio de los colores
+  static const Color primaryNavy = Color(0xFF1E3A8A);
+  static const Color primaryLight = Color(0xFF3B82F6);
+  static const Color primaryDark = Color(0xFF1E40AF);
+  static const Color secondaryGray = Color(0xFF6B7280);
+  static const Color grayLight = Color(0xFF9CA3AF);
+  static const Color grayDart = Color(0xFF374151);
+  static const Color backgroundGray = Color(0xFFF9FAFB);
+
+  //declaramos el inicio de la app q contendra todos los elementos
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
