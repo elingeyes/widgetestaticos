63810680e0b4e57b2b036c4ff1072c6ca87180f4
Alexander <alexandersilva580@outlook.com>
2025-07-28 21:26:29 -0400

    contenedor con icono de person_add

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+// importacion de material
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// declaracion de MyApp para
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Formulario de Registro',
+      // declaracion del tema para el formulario
       theme: ThemeData(
         primarySwatch: Colors.red,
         primaryColor: Color(0xFFBF3604),
@@ -27,7 +30,7 @@ class MyApp extends StatelessWidget {
 }
 
 class RegistrationFromScreen extends StatelessWidget {
-    // definir los colores para la paleta
+    // definir la paleta de colores para la paleta
     static const Color primaryNavy = Color(0xFF590902);
     static const Color primaryLight = Color(0xFFBF3604);
     static const Color primaryDark = Color(0xFFF27405);
@@ -38,21 +41,61 @@ class RegistrationFromScreen extends StatelessWidget {
 
     @override
   Widget build(BuildContext context) {
+      // contenedor principal de el contenido de la app
     return Scaffold(
+      // establecer el fondo de pantalla
       backgroundColor: backgroundGray,
       appBar: AppBar(
+        // definimos el titulo principal
         title: Text(
           'Registro de Usuario',
             style: TextStyle(
+              // el color del titulo
               color: Colors.white,
+              // estilo del titulo
               fontWeight: FontWeight.bold,
+              // tamaño del titulo
               fontSize: 20,
             ),
         ),
+        // establecemos el color del contendor del texto
         backgroundColor: primaryNavy,
         elevation: 2,
-        centerTitle: true,
+        centerTitle: true, // centramos el titulo
       ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // contenedor para la seccion
+              Container(
+                padding: EdgeInsets.all(24),
+                  decoration: BoxDecoration(
+                    color: Colors.white, // color del contenedor
+                    borderRadius: BorderRadius.circular(12), // bordes del contenedor
+                    boxShadow: [
+                      BoxShadow(
+                        color: secondaryGray, // leve sombra para dar efecto flotante
+                        blurRadius: 10,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                child: Column(
+                  children: [
+                    // establecemos el icono
+                    Icon(
+                      Icons.person_add, // llamamos al icono para el contenedor
+                      size: 48, // definimos el tamaño para el icono
+                      color: primaryNavy, // asignamos color al icono
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
     );
   }
 }
